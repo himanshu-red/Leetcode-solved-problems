4f1209bf57ab06683c8d1f0ce86b93ea3f11c394
himanshu-red <45516142+himanshu-red@users.noreply.github.com>
2021-12-26 14:20:31 +0530

    Update rotateImage_leetcode.cpp

--- rotateImage_leetcode.cpp
+++ rotateImage_leetcode.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     void rotate(vector<vector<int>>& v) {
-			  int n = v.size(); 
-			 for (int i = 0; i < n; i++)
-				{
-						for (int j = 0; j < i; j++)
-						{
-								int temp = v[i][j]; 
-								v[i][j] = v[j][i]; 
-								v[j][i] = temp; 
-						}
-				}
+          int n = v.size(); 
+         for (int i = 0; i < n; i++)
+        {
+                for (int j = 0; j < i; j++)
+                {
+                        int temp = v[i][j]; 
+                        v[i][j] = v[j][i]; 
+                        v[j][i] = temp; 
+                }
+        }
 
-			for (int i = 0; i < n; i++)
-			{
-					for (int j = 0; j < n/2; j++)
-					{
-							int temp = v[i][n-1-j];
-							v[i][n-1-j] = v[i][j];
-							v[i][j] = temp;
-					}
-			}
+        for (int i = 0; i < n; i++)
+        {
+                for (int j = 0; j < n/2; j++)
+                {
+                        int temp = v[i][n-1-j];
+                        v[i][n-1-j] = v[i][j];
+                        v[i][j] = temp;
+                }
+        }
     }
 };
