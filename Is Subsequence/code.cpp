9fce56f574a55c08b185a3a7f6eec6fb4ba85cd0
himanshu-red <45516142+himanshu-red@users.noreply.github.com>
2022-01-02 19:23:33 +0530

    Create code.cpp

---
+++ Is Subsequence/code.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int j = 0; 
+        if (s.size() == 0)
+            return true; 
+        for (int i =0 ; i< t.size(); i++)
+        {
+            if (s[j] == t[i] && j < s.size())
+                j++;
+            if (j == s.size())
+                return true; 
+        }
+        
+        return false; 
+    }
+};
