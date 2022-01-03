7b852bfd51f91577058de4acd6cc80514822f770
himanshu-red <45516142+himanshu-red@users.noreply.github.com>
2022-01-03 09:22:04 +0530

    Create code.cpp

---
+++ Merge sorted arrays/code.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        vector<int> res; 
+        int i = 0; 
+        int j = 0; 
+        nums1.erase(nums1.end()-(nums2.size()) , nums1.end()) ; 
+        while (i < nums1.size() && j < nums2.size())
+        {
+            
+            if (nums1[i] <= nums2[j])
+            {
+                res.push_back(nums1[i]); 
+                i++; 
+            }
+             else if (nums1[i] > nums2[j])
+            {
+                res.push_back(nums2[j]); 
+                j++; 
+            }
+        }
+        while (i < nums1.size())
+        {
+            res.push_back(nums1[i]); 
+            i++; 
+        }
+         while (j < nums2.size())
+        {
+            res.push_back(nums2[j]); 
+            j++; 
+        }
+        nums1 = res; 
+    }
+};
