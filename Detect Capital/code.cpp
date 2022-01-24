3fa840d23fcd30197e8598d5c7e1ab1383d99fc5
HIMANSHU RANJAN <45516142+himanshu-red@users.noreply.github.com>
2022-01-24 18:11:00 +0530

    Update code.cpp

--- Detect Capital/code.cpp
+++ Detect Capital/code.cpp
@@ -9,7 +9,6 @@ public:
         bool firstLetter = checkCaps(word[0]); 
         for (int i = 0; i< word.size(); i++)
         {
-            
             if (checkCaps(word[i])) capsCount++; 
         }
         if (!firstLetter && capsCount == 0) return true; 
