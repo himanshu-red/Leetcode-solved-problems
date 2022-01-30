6c3f8c8e5a500e64c0b238d3db01e0c241b35209
HIMANSHU RANJAN <45516142+himanshu-red@users.noreply.github.com>
2022-01-30 09:25:34 +0530

    Create code.cpp

---
+++ All the divisions with the highest score of binary arrays/code.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    vector<int> maxScoreIndices(vector<int>& nums) {
+        int totalOnes = 0; 
+        int totalZeroes = 0; 
+        int leftZeroes= 0; 
+        int rightOnes = 0; 
+        int maxScore = 0; 
+        vector<int> indices; 
+        for (auto &i : nums)
+        {
+            if (i == 1) totalOnes++; 
+            else        totalZeroes++; 
+        }
+        for (int i = 0; i< nums.size(); i++)
+        {
+            if (i == 0)
+            {
+                leftZeroes = 0; 
+                rightOnes= totalOnes; 
+            }
+            else if (i>0)
+            {
+                if (nums[i-1] == 0)
+                    leftZeroes++;
+                else if (nums[i-1] == 1)
+                    rightOnes--; 
+
+                    
+            }
+            if (maxScore < leftZeroes+rightOnes)
+            {
+                maxScore = leftZeroes + rightOnes; 
+                indices.clear(); 
+                indices.push_back(i); 
+            }
+            else if (maxScore == leftZeroes + rightOnes)
+            {
+                indices.push_back(i); 
+            }
+
+        }
+        if (maxScore < totalZeroes)
+        {
+            indices.clear(); 
+            indices.push_back(nums.size()); 
+        }
+        else if (maxScore == totalZeroes)
+        {
+            indices.push_back(nums.size()); 
+        }
+        return indices; 
+    }
+};
