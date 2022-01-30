f60258c7da7ddca7170f259ea7b4bc2c5971be23
HIMANSHU RANJAN <45516142+himanshu-red@users.noreply.github.com>
2022-01-30 09:24:29 +0530

    Create code.cpp

---
+++ Keep multiplying found values by 2/code.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        sort(nums.begin(), nums.end()); 
+        while (binary_search(nums.begin(), nums.end(), original))
+        {
+            original *= 2; 
+        }
+        return original; 
+    }
+};
